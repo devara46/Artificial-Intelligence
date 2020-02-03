181dcf1dac35311a8e3071585c8dce9f2800711d
devara46 <57223013+devara46@users.noreply.github.com>
2020-02-03 22:44:21 +0900

    Reorganize and add some comment
    Reorganize the file
    Add some comment to make it easier to learn

--- AI_Search/UninformedSearch.py
+++ AI_Search/UninformedSearch.py
@@ -4,10 +4,14 @@
 # In[1]:
 
 
+# heappush -> Push the value item onto the heap
+# heappop -> Pop and return the smallest item from the heap
 from queue import heappop, heappush
 from math import inf
+# deque(deck) -> double ended queue
 from collections import deque
 import pandas as pd
+# networkx-> Python package for the creation, manipulation, and study of the structure, dynamics, and functions of complex networks
 import networkx as nx
 import matplotlib.pyplot as plt
 import warnings
@@ -37,26 +41,36 @@ class Graph:
         except: return inf
 
     def breadth_first_search(self, start, goal):
+        # Initial state
         found, fringe, visited, came_from = False, deque([start]), set([start]), {start: None}
+        # Cosmetic
         print('{:15s} | {}'.format('Expand Node', 'Fringe'))
         print('-------------------------')
         print('{:15s} | {}'.format('-', start))
+        # Step
         while not found and len(fringe):
             current = fringe.pop()
             print('{:15s}'.format(current), end=' | ')
             if current == goal: found = True; break
             for node in self.neighbors(current):
-                if node not in visited: visited.add(node); fringe.appendleft(node); came_from[node] = current
+                if node not in visited: 
+                    visited.add(node)
+                    fringe.appendleft(node)
+                    came_from[node] = current
             print(', '.join(fringe))
+        # Result
         if found: print(); return came_from
         else: print('No path from {} to {}'.format(start, goal))
     
     def depth_limited_search(self, start, goal, limit=-1):
         print('Depth limit =', limit)
+        # Initial State
         found, fringe, visited, came_from = False, deque([(0, start)]), set([start]), {start: None}
+        # Cosmetic
         print('{:15s} | {}'.format('Expand Node', 'Fringe'))
         print('-------------------------')
         print('{:15s} | {}'.format('-', start))
+        # Step
         while not found and len(fringe):
             depth, current = fringe.pop()
             print('{:15s}'.format(current), end=' | ')
@@ -64,17 +78,22 @@ class Graph:
             if limit == -1 or depth < limit:
                 for node in self.neighbors(current):
                     if node not in visited:
-                        visited.add(node); fringe.append((depth + 1, node))
+                        visited.add(node)
+                        fringe.append((depth + 1, node))
                         came_from[node] = current
             print(', '.join([n for _, n in fringe]))
+        # Result
         if found: print(); return came_from
         else: print('No path from {} to {}'.format(start, goal))
 
     def uniform_cost_search(self, start, goal):
+        # Initial State
         found, fringe, visited, came_from, cost_so_far = False, [(0, start)], set([start]), {start: None}, {start: 0}
+        # Cosmetic
         print('{:15s} | {}'.format('Expand Node', 'Fringe'))
         print('-------------------------')
         print('{:15s} | {}'.format('-', str((0, start))))
+        # Step
         while not found and len(fringe):
             _, current = heappop(fringe)
             print('{:15s}'.format(current), end=' | ')
@@ -82,13 +101,17 @@ class Graph:
             for node in self.neighbors(current):
                 new_cost = cost_so_far[current] + self.cost(current, node)
                 if node not in visited or cost_so_far[node] > new_cost:
-                    visited.add(node); came_from[node] = current; cost_so_far[node] = new_cost
+                    visited.add(node)
+                    came_from[node] = current
+                    cost_so_far[node] = new_cost
                     heappush(fringe, (new_cost, node))
             print(', '.join([str(n) for n in fringe]))
+        # Result
         if found: print(); return came_from, cost_so_far[goal]
         else: print('No path from {} to {}'.format(start, goal)); return None, inf
 
     @staticmethod
+    # Print the path
     def print_path(came_from, goal):
         parent = came_from[goal]
         if parent:
@@ -96,7 +119,7 @@ class Graph:
         else: print(goal, end='');return
         print(' =>', goal, end='')
 
-
+    # Override class to make it print words instead of memory reference
     def __str__(self):
         return str(self.edges)
 
@@ -104,20 +127,26 @@ class Graph:
 # In[3]:
 
 
+# Stop showing warning
 warnings.filterwarnings('ignore')
 
-df_nodes = pd.read_csv('D:/Python/AI_Search/romania_nodes.csv')
-df_edges = pd.read_csv('D:/Python/AI_Search/romania_edges.csv')
+# Load csv
+df_nodes = pd.read_csv('.../Dataset/romania_nodes.csv')
+df_edges = pd.read_csv('.../Dataset/romania_edges.csv')
 
 G = nx.Graph()
 graph = Graph(directed=True)
+
+# Load node
 for index, row in df_nodes.iterrows():
     G.add_node(row['nodes'])
     
+# Load edge and add edge into Graph class
 for index, row in df_edges.iterrows():
     G.add_weighted_edges_from([(row['source'], row['target'], row['weight'])])
     graph.add_edge(row['source'], row['target'], row['weight'])
 
+# Settings for graph
 options = {
     'edge_color': '#FFDEA2',
     'width': 1,
@@ -125,6 +154,7 @@ options = {
     'font_weight': 'regular',
 }    
 
+# Print graph or network
 nx.draw(G, **options)
 ax = plt.gca()
 plt.show()
@@ -133,13 +163,20 @@ plt.show()
 # In[4]:
 
 
+# Define Starting node, Goal node, and Depth limit for dlm
 start, goal, l = 'Arad', 'Bucharest', 5
+
+# Run and print bfs
 print('Breadth First Search'); print('-------------------------'); 
 bfs_path = graph.breadth_first_search(start, goal)
 if (bfs_path): print('Path:', end=' '); Graph.print_path(bfs_path, goal); print();print()
+
+# Run and print dlm
 print('Depth Limited Search'); print('-------------------------'); 
 dlm_path = graph.depth_limited_search(start, goal, l)
 if (dlm_path): print('Path:', end=' '); Graph.print_path(dlm_path, goal); print();print()
+
+# Run and print dlm
 print('Uniform Cost Search'); print('-------------------------'); 
 ucs_path, cost = graph.uniform_cost_search(start, goal)
 if (ucs_path): print('Path:', end=' '); Graph.print_path(ucs_path, goal); print('\nCost:', cost)
